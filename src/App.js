d0739a3d03f7db9c79a3a098bdb635d1f883edf4
Everett Bazzocchi <contact@everettbazzocchi.ca>
2025-03-06 14:26:53 -0600

    added appwrite backend

--- src/App.js
+++ src/App.js
@@ -1,14 +1,54 @@
 import React from 'react';
+// import appwrite
+import appwrite from 'appwrite';
 
 import BarItem from './components/BarItem';
 import Footer from './components/Footer';
 import Header from './components/Header';
 
-import categories from './data/categories.json';
-import items from './data/items.json';
-
 
 export default function App() {
+
+    // Init appwrite
+    const client = new appwrite.Client({
+        endpoint: "https://api.cloud.shotty.tech/v1",
+        project: "67c9ff7a0013c21e2b40",
+    });
+
+    // Init database
+    const database = new appwrite.Database(client);
+
+    // db id 67c9ffd9003d68236514
+    // items collection id 67c9ffe6001c17071bb7
+    // category collection id 67c9ffdd0039c4e09c9a
+    const categories = [];
+    const items = [];
+
+    const itemsSubscription = database.subscribe([
+        "databases.67c9ffd9003d68236514.collections.67c9ffe6001c17071bb7.documents",
+    ]);
+    const categoriesSubscription = database.subscribe([
+        "databases.67c9ffd9003d68236514.collections.67c9ffdd0039c4e09c9a.documents",
+    ]);
+
+    categoriesSubscription.then(() => {
+        database.listDocuments('67c9ffdd0039c4e09c9a').then((response) => {
+            categories.length = 0;
+            response.documents.forEach((category) => {
+                categories.push(category);
+            });
+        });
+    });
+    itemsSubscription.then(() => {
+        database.listDocuments('67c9ffe6001c17071bb7').then((response) => {
+            items.length = 0;
+            response.documents.forEach((item) => {
+                items.push(item);
+            });
+        });
+    });
+
+
 
     return (
         <content onClick={() => document.documentElement.requestFullscreen()}>
