b0770b85270d1e5333eb6ee59c528f03c11e453b
everettbazzocchi <contact@everettbazzocchi.ca>
2025-10-07 16:22:19 -0500

    fixed bar times

--- src/App.js
+++ src/App.js
@@ -8,12 +8,16 @@ import Header from './components/Header';
 export default function App() {
     const { categories, items, settings } = useAppwrite();
     const hour = new Date().getHours();
-    const [alcoholStart, alcoholEnd] = settings ? [settings.bar_start, settings.bar_end] : [15, 2];
-    console.log(alcoholStart, alcoholEnd);
-    console.log(hour);
-    console.log((hour >= alcoholStart && hour < alcoholEnd))
 
-    const alcoholEnabled = (hour >= alcoholStart && hour < alcoholEnd);
+    const [rawStart, rawEnd] = settings ? [settings.bar_start, settings.bar_end] : [15, 2];
+    const alcoholStart = Number(rawStart ?? 15);
+    const alcoholEnd = Number(rawEnd ?? 2);
+
+    const normHour = (h) => (((Number(h) || 0) % 24) + 24) % 24;
+    const s = normHour(alcoholStart);
+    const e = normHour(alcoholEnd);
+
+    const alcoholEnabled = s <= e ? (hour >= s && hour < e) : (hour >= s || hour < e);
 
     return (
         <div id='menu' onClick={() => document.documentElement.requestFullscreen()}>
